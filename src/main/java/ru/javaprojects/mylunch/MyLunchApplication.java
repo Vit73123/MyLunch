d4676b313eb05d693af81dea2b22d8ab0ad3aa1f
vit73123 <vit73123@gmail.com>
2024-12-25 12:38:32 +0300

    init

---
+++ src/main/java/ru/javaprojects/mylunch/MyLunchApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.mylunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javaprojects/mylunch/MyLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.mylunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
